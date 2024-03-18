ec7589a2ce1ce32289c1d725d2ebf4028f4ef848
Nukeem77 <134224476+Nukeen77@users.noreply.github.com>
2024-03-18 19:18:09 -0400

    Add files via upload

---
+++ sites/index.js
@@ -0,0 +1,19 @@
+function loadDisney() {
+  window.location.replace('');
+}
+
+function loadWebtoon() {
+  window.location.replace('https://lumanom.v10-13.repl.co/service/hvtrs8%2F-wuw%2Cwgbvomnq.aoo%2Fgn-');
+}
+
+function loadNetflix() {
+  window.location.replace('https://lumanom.v10-13.repl.co/service/hvtrs8%2F-wuw%2Cngtdlkx%2Ccmm-');
+}
+
+function loadTwitch() {
+  window.location.replace('https://lumanom.v10-13.repl.co/service/hvtrs8%2F-wuw%2Ctuivcj.vv-');
+}
+
+function loadPinterest() {
+  window.location.replace('https://lumanom.v10-13.repl.co/service/hvtrs8%2F-wuw%2Cpknvepeqt%2Ccmm-');
+}
